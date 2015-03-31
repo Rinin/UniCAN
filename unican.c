0e4a123e30fe4e6511dc566ad92d6f4a22f7b256
vlaskin <vlaskin>
2015-03-31 10:13:26 +0000

    RX related part

--- unican.c
+++ unican.c
@@ -1,29 +1,72 @@
 #include "unican.h"
+#include <stdlib.h>
+//TODO check all malloc results
 
+static unican_status state;
 static unican_buffer unican_rx_buffers [UNICAN_RX_BUFFERS_COUNT];
+
 /*
 Initialization of buffers itself
 */
 //TODO: Implementation needed
 uint16 unican_init (void)
 {
-  uint16 i;
-  
+  uint16 i;  
   for (i=0; i < UNICAN_RX_BUFFERS_COUNT; i++)
-    unican_rx_buffers[i].umsg.unican_address_from = 0;
-    
+    unican_rx_buffers[i].umsg = NULL;
 }
+
+/*
+Allocate memory for new unican_message
+*/
+inline static unican_message* unican_allocate_message(uint16 msg_id, uint16 from, uint16 to, uint16 data_len)
+{
+  if (state.free_space > data_len+sizeof(unican_message))
+    return NULL;
+  else
+  {
+  unican_message* temp_msg;
+  temp_msg = malloc (sizeof(unican_message));
+  if (temp_msg == NULL)
+    return NULL;
+  
+  temp_msg->unican_msg_id = msg_id;
+  temp_msg->unican_address_from = from;
+  temp_msg->unican_address_to = to;
+  temp_msg->unican_length = data_len;
+  temp_msg->data = malloc (data_len);
+  if (temp_msg->data == NULL)
+  {
+    free(temp_msg);
+    return NULL; 
+  } 
+  temp_msg->next_msg = NULL;
+  state.free_space -= (data_len+sizeof(unican_message));
+  return temp_msg;
+  }
+}
+
+        
+
 /*
 Find buffer associated with data flow from known address.
 */
 static unican_buffer* unican_find_buffer (uint16 address_from)
 {
   uint16 i;
+  unican_buffer* free_buffer;
+  free_buffer = NULL;
+  
   for (i=0; i < UNICAN_RX_BUFFERS_COUNT; i++)
-    if (unican_rx_buffers[i].umsg.unican_address_from == address_from)
-      return &(unican_rx_buffers[i]);
+    if (unican_rx_buffers[i].umsg != NULL)
+    {
+      if (unican_rx_buffers[i].umsg->unican_address_from == address_from)
+        return &(unican_rx_buffers[i]);
+    }
+    else
+      free_buffer = &(unican_rx_buffers[i]);
       
-  return NULL;
+  return free_buffer;
 }
 
 /*
@@ -33,13 +76,35 @@ Check if identifier valid or not
 static uint16 can_check_message (can_message* msg)
 {
   if (msg->can_dlc > CAN_MAX_DLC)
-    return CAN_MESSAGE_TOO_LONG;
+    return UNICAN_CAN_MESSAGE_TOO_LONG;
   else if ((msg->can_identifier > 0x4FF) && (msg->can_extbit == 0))
-    return CAN_IDENTIFIER_TOO_LONG ;
+    return UNICAN_CAN_IDENTIFIER_TOO_LONG ;
   else if ((msg->can_identifier > 0x1FFFFFFF) && (msg->can_extbit > 0))
-    return CAN_IDENTIFIER_EXT_TOO_LONG ;
+    return UNICAN_CAN_IDENTIFIER_EXT_TOO_LONG ;
   else
-    return 0;
+    return UNICAN_OK;
+}
+
+
+/*
+Saves valid unican messages for further use
+*/
+static void unican_save_message (unican_message* umsg)
+{
+//TODO:IMPLEMENT QUEUE!
+  if (state.last == NULL)
+      state.last = umsg;
+    else
+      state.last->next_msg = umsg;
+  state.messages_count++;
+  
+}
+/*
+
+*/
+static uint16 unican_check_crc(uint8* buff, uint16 length)
+{
+  //TODO:Implement
 }
 
 /*
@@ -50,13 +115,20 @@ used buffer
 static uint16 unican_collect_buffer(unican_buffer* buff)
 {
   
+  uint16 crc;
+  crc = unican_check_crc(buff->umsg->data, buff->umsg->unican_length);
+  //TODO:Check actually
+  unican_save_message(buff->umsg);
+  buff->umsg = NULL;
+  buff->crc = 0x0000;
+  buff->position = 0;
+  state.free_buffers_count++;
+  return UNICAN_OK;
 }
 
 /*
 Processes single received CAN message 
 */
-//TODO:Replace return with some other errcode handler. We shouldn't return to user
-//TODO:Add buffer beteen irq and receive;
 
 uint16 can_receive_message (can_message* msg)
 {
@@ -66,6 +138,8 @@ uint16 can_receive_message (can_message* msg)
   uint16 address_from = 0;
   uint8 data_bit = 0;
   uint16 i;
+  uint16 retval;
+  retval = UNICAN_OK;
   
   if (msg->can_extbit == 0) 
   {
@@ -83,7 +157,7 @@ uint16 can_receive_message (can_message* msg)
   //Checking message
   uint16 errcode;
   errcode = can_check_message(msg);
-  if (errcode != 0)
+  if (errcode != UNICAN_OK)
     return errcode;
   //Done with checking
   
@@ -95,19 +169,24 @@ uint16 can_receive_message (can_message* msg)
     buff = unican_find_buffer(address_from);
     if (buff == NULL)
       return UNICAN_DATA_WITHOUT_START;
+    else if (buff->umsg == NULL)
+      return UNICAN_DATA_WITHOUT_START;
     else
     {
       for (i=0 ; i < msg->can_dlc ; i++)
       {
-        if (buff->position < buff->umsg.unican_length)
+        if (buff->position < buff->umsg->unican_length)
         {
-          buff->umsg.data[buff->position] = msg->data[i];
+          buff->umsg->data[buff->position] = msg->data[i];
           buff->position++;
         }
         else
-          return UNICAN_UNEXPECTED_DATA;
+        {
+          unican_collect_buffer(buff);
+          return UNICAN_WARNING_UNEXPECTED_DATA;//and what to do with buffer?
+        }
       }
-      if (buff->umsg.unican_length == buff->position)
+      if (buff->umsg->unican_length == buff->position)
         unican_collect_buffer(buff);
     }
   }
@@ -117,27 +196,59 @@ uint16 can_receive_message (can_message* msg)
   {
     uint16 msg_id;
     if (msg->can_dlc >= CAN_MIN_DLC)
-      msg_id = ((msg->data[0]&0x00FF) | (msg->data[1]<<8)); 
+      msg_id = msg->data[0] + (msg->data[1] * 256); 
     else
-      return CAN_MESSAGE_TOO_SHORT;
+      return UNICAN_CAN_MESSAGE_TOO_SHORT;
     
     switch (msg_id)
     {
       //Some protocol-specific short messages
       case UNICAN_START_LONG_MESSAGE:
       {
-        //TODO: Implementation needed
+        if (msg->can_dlc < UNICAN_HEADER_SIZE)
+          return UNICAN_HEADER_TOO_SHORT;
+          
+        buff = unican_find_buffer(address_from);
+        if (buff == NULL)
+          return UNICAN_NO_FREE_BUFFER;
+        else 
+        {
+          if (buff->umsg != NULL) 
+          {         
+            unican_drop_message(buff->umsg);
+            retval = UNICAN_WARNING_BUFFER_OVERWRITE;
+          }
+          uint16 msg_subid = msg->data[2] + (msg->data[3] * 256);
+          uint16 data_length = msg->data[6] + (msg->data[7] * 256);          
+          buff->umsg = unican_allocate_message(msg_subid, address_from, address_to, data_length);
+          if (buff->umsg == NULL)          
+            return UNICAN_CANT_ALLOCATE_MESSAGE;//FIXME:Here we have a chance to ignore retval
+          state.free_buffers_count--;
+          buff->crc = msg->data[4] + (msg->data[5] * 256);
+          buff->position = 0;
+        }
+        
       } break;
       //Done with protocol-specific short messages
       //Regular short messages
       default:
-      {
-        //TODO: Implementation needed
+      {        
+        unican_message* temp_msg;
+        temp_msg = unican_allocate_message(msg_id, address_from, address_to, msg->can_dlc - CAN_MIN_DLC);
+        if (temp_msg == NULL)
+          return UNICAN_CANT_ALLOCATE_MESSAGE;
+        
+        for ( i = 0; i<temp_msg->unican_length; i++)
+          temp_msg->data[i]=msg->data[i+CAN_MIN_DLC];
+          
+        unican_save_message (temp_msg);          
+          
       } break;
       //Done with regular short messages
-    }    
+    }
   }
   //Done with short commands
+  return retval;
 }
 
 inline void can_send_message (can_message* msg)
@@ -145,8 +256,12 @@ inline void can_send_message (can_message* msg)
   can_HW_send_message (msg);
 }
 
-//TODO: Implementation needed
-void unican_drop_msg(unican_message* msg)
+void unican_drop_message(unican_message* msg)
 {
-
+  if (msg != NULL)
+  {
+    state.free_space += msg->unican_length+sizeof(unican_message);
+    free(msg->data);
+    free(msg);
+  }
 }
--- unican.h
+++ unican.h
@@ -4,9 +4,25 @@
 #include "unican_errcodes.h"
 #include "unican_msg_id.h"
 
-#define NULL 0
+#ifndef NULL
+#define NULL (void*)0
+#endif /* NULL */
+
 #define CAN_MAX_DLC 8 //maximum value of data length code
 #define CAN_MIN_DLC 2 //minimum value of data length code
+#define UNICAN_HEADER_SIZE 8
+typedef struct tag_unican_message unican_message;
+
+typedef struct tag_unican_status
+{
+  uint8 is_online;
+  uint32 free_space; //Free space reserved for unican messages //TODO:init it
+  uint32 messages_count;
+  uint16 free_buffers_count;
+  unican_message* first;
+  unican_message* last;
+  
+}  __attribute__ ((packed)) unican_status;
 
 typedef struct tag_can_message
 {
@@ -17,18 +33,19 @@ typedef struct tag_can_message
   volatile uint8 data[8]; //Data field
 }  __attribute__ ((packed)) can_message;
 
-typedef struct tag_unican_message
+struct tag_unican_message
 {
   uint16 unican_msg_id; //MSG_ID of unican message
   uint16 unican_address_from; // address of sender in sattelite network
   uint16 unican_address_to; // address of receiver in sattelite network
   uint16 unican_length; //length of data 		
-  volatile uint8* data; //pointer to data field
-}  __attribute__ ((packed)) unican_message;
+  uint8* data; //pointer to data field
+  unican_message* next_msg; //pointer to next_message
+}  __attribute__ ((packed));
 
 typedef struct tag_unican_buffer
 {
-  unican_message umsg; //MSG accamulated in buffer
+  unican_message* umsg; //MSG accamulated in buffer
   uint16 crc; // CRC of message, 0 for short messages
   uint16 position; //Current position in buffer
 }  __attribute__ ((packed)) unican_buffer;
@@ -43,7 +60,6 @@ inline void can_send_message (can_message* msg);
 void unican_proceed_message (unican_message* msg);
 void unican_send_message (unican_message* msg);
 
-void unican_create_msg(unican_message* msg);
-void unican_drop_msg(unican_message* msg);
+void unican_drop_message(unican_message* msg);
 
 #endif /* UNICAN_H_ */
